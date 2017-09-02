195703c10db4fabb52bf3cca00f34f440c584dd0
HuuBaa <742790905@qq.com>
2017-09-02 08:38:57 +0800

    heroku

--- config.py
+++ config.py
@@ -2,13 +2,14 @@ import os
 basedir=os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY='huu cool'
+    # SSL_DISABLE = True
     FLASK_MAIL_SUBJECT_PREFIX='[Flasky]'
     FLASK_MAIL_SENDER='Huu Flask <742790905@qq.com>'
     FLASKY_ADMIN=os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     MAIL_SERVER='smtp.qq.com'
-    MAIL_PORT=465
-    MAIL_USE_SSL=True
+    MAIL_PORT=587   
+    MAIL_USE_TLS = True
     MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
     FLASKY_POST_PER_PAGE=10
@@ -22,18 +23,20 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG=True
-    SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir,'data-develop.sqlite')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DEV_DATABASE_URL') or'sqlite:///' + os.path.join(basedir,'data-develop.sqlite')
 
+    
 
 class TestingConfig(Config):
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     WTF_CSRF_ENABLED=False
     TESTING=True
-    SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir,'data-test.sqlite')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('TEST_DATABASE_URL') or'sqlite:///' + os.path.join(basedir,'data-test.sqlite')
+
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI='sqlite:///' + os.path.join(basedir,'data.sqlite')
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir,'data.sqlite')
 
     @classmethod
     def init_app(cls,app):
@@ -44,8 +47,8 @@ class ProductionConfig(Config):
         credentials=None
         secure=None
         if getattr(cls,'MAIL_USERNAME',None) is not None:
-            credentials=(cls.MAIL_USERNAME,cls.MAIL_PASSWORD)
-            if getattr(cls,'MAIL_USE_SSL',None):
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls,'MAIL_USE_TLS',None):
                 secure=()
         mail_handler=SMTPHandler(
             mailhost=('smtp.qq.com',587),
@@ -55,13 +58,24 @@ class ProductionConfig(Config):
             credentials=credentials,
             secure=secure
             )
-        mail_handler.setLevel(logging.DEBUG)
+        mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
+class HerokuConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        import logging
+        from logging import StreamHandler
+        file_handler=SMTPHandler()
+        file_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(file_handler)
 
 config={
     'development':DevelopmentConfig,
     'testing':TestingConfig,
     'production':ProductionConfig,
-    'default':DevelopmentConfig
+    'default':DevelopmentConfig,
+    'heroku':HerokuConfig
 }
